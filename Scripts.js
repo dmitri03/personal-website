adae8215954814e1fd8593a504b5cac5ba35b6ac
dmitri03 <dimony.03@gmail.com>
2022-04-17 23:05:34 -0700

    commit

--- Scripts.js
+++ Scripts.js
@@ -1,124 +1,45 @@
 
 window.addEventListener('load', (event) => {
-    // console.log(event);
-
-    // //******************************
-    // //mouseover, mouseout, and click
 
     elements = document.getElementsByClassName("hobbyDiv");
     for(var i = 0; i < elements.length; i++){
-             elements[i].addEventListener("mouseover", onHoverOn);
-             elements[i].addEventListener("mouseout", onHoverOff);
-         }
+             elements[i].addEventListener("click", onClick);
+    }
 
-    elements.addEventListener("mouseover", onHoverOn);
-    document.addEventListener("mouseout", onHoverOff);
-     
-    function onHoverOn(event){
-        event.target.style.color="blue";
-        txtDiv = document.createElement('div');
-        txtDiv.innerText = "shoe";
-        document.getElementsByClassName("hobbyDiv").item(0).innerHTML='<p>Shoe</p>';
-        console.log(event.target);
+    
+    function onClick(event){
+  
+        if(event.target.id=='soccerImg'){
+            if(event.target.state='0'){
+            document.getElementsByClassName("hobbyDiv").item(2).innerHTML='<p>Soccer</p>';
+            event.target.state='1';
+            }else{
+                document.getElementsByClassName("hobbyDiv").item(2).innerHTML='<img class="hobby-image"id="soccerImg" src="images/001-soccer.png" alt="iphone-mockup">';
+                event.target.state='0';
+            }
+        console.log(event.target.id);}
+        else if(event.target.id=='chessImg'){
+            if(event.target.state='0'){
+            document.getElementsByClassName("hobbyDiv").item(0).innerHTML='<p>Chess</p>';
+            event.target.state='1';
+            }else{
+                document.getElementsByClassName("hobbyDiv").item(0).innerHTML=' <img class="hobby-image" id="chessImg"src="images/001-chess.png" alt="iphone-mockup">';
+                event.target.state='0';
+            }
+        }
+        else{
+            if(event.target.state='0'){
+            document.getElementsByClassName("hobbyDiv").item(1).innerHTML='<p>Drawing</p>';
+            event.target.state='1';
+            }else{
+                document.getElementsByClassName("hobbyDiv").item(1).innerHTML='<img class="hobby-image"id="pencilImg" src="images/001-pencil.png" alt="iphone-mockup">';
+                event.target.state='0';
+            }
+        }
     };  
-    function onHoverOff(event){
-        event.target.style.color= "";
-        console.log("off")
-    }; 
 
 
   
 
 });
-    // function onClickGreen(event){
-    //     event.target.style.color="green"
-    // };
     
-    // elements = document.getElementsByTagName("h2");
-    // for(var i = 0; i < elements.length; i++){
-    //     elements[i].addEventListener("click", onClickGreen);
-    // }
-
-
-    // //******************
-    // //keydown v.s. keyup
-
-    // document.addEventListener('keydown', logKey);
-    // document.addEventListener('keyup', logKey);
-
-    // function logKey(e) {
-    //   console.log(e.type, e.code);
-    // }
-
-
-    // //*******************************************
-    // //customizable global event listener function
-    // //
-
-    // function addGlobalEventListener(type, selector, func){
-    //     document.addEventListener(type, (event) => {
-    //         if (event.target.matches(selector)){
-    //             func(event)
-    //         }
-    //     })
-    // }
-
-    // addGlobalEventListener("click", ".hero-image", (e)=>{ console.log("hi")})
-
-    // //************************************************************************
-    // //A brief interlude on callbacks, anonymous functions, and arrow functions
-
-    // function callFunctionOnZero(func){
-    //     return func(0)
-    // }
-
-    // callFunctionOnZero(console.log);
-    // callFunctionOnZero(function(arg){console.log(arg+1)});
-    // callFunctionOnZero((arg)=>{console.log(arg+2)});
-    // callFunctionOnZero(arg => console.log(arg+3));
-
-    // //returns / console.logs
-    // console.log(callFunctionOnZero(arg=>{console.log(arg+4)}))
-    // console.log(callFunctionOnZero(arg=>arg+5))
-
-    // // **************
-    // // HTML Injection
-
-    // main = document.getElementById('main');
-    // newDiv = document.createElement('div');
-    // newDiv.innerText = 'Hello'; //these all conflict, which ever happens last wins
-    // // newDiv.innerText = '<p>hi</p>'; //this will be text not HTML so you will se <p> on the page
-    // main.append(newDiv);
-    // main.append("some text");
-    // main.append(newDiv); // it moves it, the specific div object can only be in one place.
-    
-    // newDiv.innerHTML = '<a href="#hello">I have a link now</a>'; //these also continue to effect the dom element after it's on the page
-    // console.log(main.children);
-    // main.insertBefore(newDiv, document.getElementById('about'));
-
-
-    // // ************
-    // // HTML Removal
-
-    // // main.remove()
-
-    // function removeTarget(event){
-    //     event.target.remove()
-    // }
-    // document.addEventListener("click",removeTarget);
-
-
-    // // **********************************
-    // // Let's build a ToDo App on our page
-
-    // //*************************************
-    // // InsertAdjacentHTML (if we have time)
-
-    // main.insertAdjacentHTML('beforebegin', '<div id="beforebegin">beforebegin</div>');
-    // main.insertAdjacentHTML('afterbegin', '<div id="afterbegin">afterbegin</div>');
-    // main.insertAdjacentHTML('beforeend', '<div id="beforeend">beforeend</div>');
-    // main.insertAdjacentHTML('afterend', '<div id="afterend">afterend</div>');
-// 
-
-
-// Stop Recording
